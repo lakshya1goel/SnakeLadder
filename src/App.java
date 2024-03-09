2867246b1331b2418f8b5a3e86d60a730c4b6981
lakshya1goel <lakshya1234goel@gmail.com>
2024-03-09 21:17:07 +0530

    add player class

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
+import player.Player;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Player p1 = new Player();
+        Player p2 = new Player();
+        p1.setPlayerDetailsFromUserInput();
+        p2.setPlayerDetailsFromUserInput();
+        p1.getPlayerDetails();
+        p2.getPlayerDetails();
     }
 }
--- src/player/Player.java
+++ src/player/Player.java
@@ -1,4 +1,5 @@
 package player;
+import java.util.Scanner;
 
 public class Player {
     String name;
@@ -7,4 +8,43 @@ public class Player {
     String address;
     int age;
 
+    public void setPlayerDetails(String name, String contactNo, String email, String address, int age) {
+        this.name = name;
+        this.contactNo = contactNo;
+        this.email = email;
+        this.address = address;
+        this.age = age;
+    }
+
+    public void setPlayerName(String name) {
+        this.name = name;
+    }
+
+    public void getPlayerDetails() {
+        System.out.println("Player Name: " + name);
+        System.out.println("Contact No: " + contactNo);
+        System.out.println("Email: " + email);
+        System.out.println("Address: " + address);
+        System.out.println("Age: " + age);
+    }
+
+    public String getPlayerName() {
+        return name;
+    }
+
+    public Player setPlayerDetailsFromUserInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Player Name: ");
+        String name = scanner.nextLine();
+        System.out.println("Enter Contact No: ");
+        String contactNo = scanner.nextLine();
+        System.out.println("Enter Email: ");
+        String email = scanner.nextLine();
+        System.out.println("Enter Address: ");
+        String address = scanner.nextLine();
+        System.out.println("Enter Age: ");
+        int age = scanner.nextInt();
+        setPlayerDetails(name, contactNo, email, address, age);
+        return this;
+    }
 }
